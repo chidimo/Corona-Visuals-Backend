3eaac99150258ac69641a1616a4a8c1daa2e881d
Chidi Orji <orjichidi95@gmail.com>
2020-04-07 00:24:00 +0100

    chore: rename secret variable

--- src/settings.js
+++ src/settings.js
@@ -2,5 +2,5 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const authorizationSecret = process.env.AUTHORIZATION_SECRET;
+export const authorizationSecret = process.env.AUTH_SECRET;
 export const connectionString = process.env.CONNECTION_STRING.trim();
